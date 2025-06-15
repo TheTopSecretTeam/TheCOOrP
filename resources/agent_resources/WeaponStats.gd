8534d2ac9949f8a4190b592a288840e1e7a71be8
FrapsSid <64778626+FrapsSid@users.noreply.github.com>
2025-06-15 18:56:13 +0300

    Add files via upload

---
+++ resources/agent_resources/WeaponStats.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name WeaponStats
+
+@export var name: String = "Basic Weapon"
+@export var base_damage: int = 10
+@export var damage_type: String = "physical"
+@export var attack_speed: int = 10
+@export var range: int = 10
+@export var sprite: Texture2D = null
