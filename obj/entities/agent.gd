83b802aa814cadced17db5b5d8f4b073c51c8ec3
FrapsSid <SidFraw@yandex.ru>
2025-06-22 15:42:38 +0300

    Sync with main

--- obj/entities/agent.gd
+++ obj/entities/agent.gd
@@ -2,67 +2,75 @@ extends PathFollow2D
 
 class_name Agent
 
-var agent_resource : Resource
+@export var agent_res : Resource
 
 # stack of rooms left to visit in a trace
 var path : Array = []
-var current_room: int
 
+@export var current_room: int
 var state : int = WANDER
-var waypoint : Node2D
 enum {
 	WANDER,
 	GOTO,
-	STANDING
+	CHAMBER,
 }
 
-@export var agent_name : String = "Brad"
-@export var speed : float = 0.1
+var waypoint : Node2D
+var flipped : bool = false:
+	set(value):
+		if value:
+			$Sprite2D.scale.x = -1
+			speed = -1 * abs(speed)
+		else:
+			$Sprite2D.scale.x = 1
+			speed = abs(speed)
+		flipped = value
+
+@export var agent_name : String = "Brad" #delete after agent resource
+@export var speed : float = 100.0
 
 func _ready() -> void:
-	$Name.text = agent_name
-	
-	
-func load_path(path_id:Array) -> void:
-	path = path_id
+	$Name.text = agent_name # change for $Name.text = agent_res.name
 
+func flip():
+	flipped = !flipped
+
+#for clicking on agents and selecting them
+func get_global_rect():
+	return $ClickRect.get_global_rect()
 
 func _on_travel():
 	if path.is_empty(): 
 		state = WANDER 
 		return
-		
-	state = GOTO
-	print(path)
+	#print(path)
 	current_room = path.pop_front()
-	
-	if path.is_empty(): 
-		state = WANDER 
+	if path.size() == 0:
+		state = WANDER
 		return
-	
 	waypoint = get_parent().get_parent().get_waypoint(path[0])
-	
+	if state == CHAMBER:
+		state = GOTO
+		waypoint.leading_room.transfer(self, current_room)
+		return
+	state = GOTO
+	flipped = (waypoint.progress - progress) < 0
+
+func _on_chamber_arrival():
+	current_room = path.pop_front()
+	path = []
+	state = CHAMBER
+	flipped = false
 
 func _process(delta: float) -> void:
 	match state:
 		WANDER:
-			progress_ratio += speed * delta
+			var prev_prog = progress
+			progress += speed * delta
+			if progress == prev_prog:
+				flip()
 		GOTO:
-			var raw_delta = waypoint.progress_ratio - progress_ratio
-			
-			var target = progress_ratio + raw_delta
-			progress_ratio = move_toward(progress_ratio, target, speed * delta)
-			
-			progress_ratio = wrapf(progress_ratio, 0.0, 1.0)
-				
-			if abs(progress_ratio - waypoint.progress_ratio) < 0.001:
-				var nextRoom = waypoint.leading_room
-				progress_ratio = nextRoom.waypoints[current_room].progress_ratio
-				
-				self.visible = false
-				self.global_position = nextRoom.global_position
-				await get_tree().process_frame
-				self.visible = true
-				
-				nextRoom.transfer(self)
-				
+			progress = move_toward(progress, waypoint.progress, abs(speed) * delta)
+			if progress == waypoint.progress:
+				#print(true)
+				waypoint.leading_room.transfer(self, current_room)
