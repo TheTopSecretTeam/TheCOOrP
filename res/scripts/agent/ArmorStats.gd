3d9b07c0280b41c434cdaed049a3721820eae3ed
FrapsSid <SidFraw@yandex.ru>
2025-06-25 18:04:18 +0300

    Finished modularity, New abno res

--- res/scripts/agent/ArmorStats.gd
+++ res/scripts/agent/ArmorStats.gd
@@ -3,7 +3,8 @@ class_name ArmorStats
 
 @export var name: String = "Basic Armor"
 @export var physical_resistance: float = 1
-@export var psychic_resistance: float = 1
+@export var mental_resistance: float = 1
 @export var sprite: Texture2D = null
+@export var special_effects_text: String
 
 @export var effects: Array[GDScript] = []
