fa0eb2f57d373a9587d0a41fa2df662383c6f4a4
corndev <corndev@mail.ru>
2025-06-25 17:12:19 +0300

    cleanup

--- obj/rooms/Cell/AnomalyWorkRes/Example/work_example.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-const PROB = 0 # work success probability
-
-func _on_button_down() -> void: 
-	var progress_bar = get_parent().get_node("CameraArea/Sprite2D/Bar")
-	progress_bar.work(PROB)
-	
-# extra functions, if any
