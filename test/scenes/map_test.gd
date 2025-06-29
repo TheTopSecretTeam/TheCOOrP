f32806eb7d5f7675a25aaef7ba16e6ac7da01c14
KarFayInn <fayzakhanovk@gmail.com>
2025-06-29 23:24:51 +0300

    suffering

--- test/scenes/map_test.gd
+++ test/scenes/map_test.gd
@@ -14,14 +14,16 @@ func test_send_agent() -> void:
 		"Jake" : [false, null],
 		"Charles" : [false, null]
 	}
-	var object = load(__source).instantiate()
-	object._ready()
+	# Create a test tree
+	var tree := SceneTree.new()
+	var object = auto_free(load(__source).instantiate())
+	tree.get_root().add_child(object)
 	var result
 	for room in 4:
 		for agent in agents.keys():
 			result = object.send_agent(agent, room)
 			if !agents[agent][0]:
-				assert_str(result).is_equal("agent_not_found")
+				assert_str(result).contains('agent_not_found')
 			else:
 				if room == agents[agent][1]:
 					assert_str(result).is_equal('success stand')
