0296ad234b31dde8bea7d809ab4c2594e4758102
corndev <corndev@mail.ru>
2025-06-30 12:54:52 +0300

    added gafur stuff

---
+++ UI/gafur_stuff/day_scene.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var finish_btn     : Button = $FinishButton
+@onready var finish_menu    : MarginContainer  = $FinishMenu
+@onready var restart_btn    : Button = $FinishMenu/VBoxContainer/RestartDayButton
+@onready var next_day_btn   : Button = $FinishMenu/VBoxContainer/NextDayButton
+func _ready():
+	Global.quota_reached.connect(show)
+	finish_btn.visible = true
+
+	finish_btn.pressed.connect(_on_finish_pressed)
+	restart_btn.pressed.connect(_on_restart_pressed)
+	next_day_btn.pressed.connect(_on_next_day_pressed)
+
+func _on_finish_pressed():
+	finish_menu.visible = true
+
+func _on_restart_pressed():
+	pass
+
+func _on_next_day_pressed():
+	pass
--- scenes/win_screen.gd
+++ scenes/win_screen.gd
@@ -2,5 +2,5 @@ extends ColorRect
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	Global.quota_reached.connect(show)
+#func _ready() -> void:
+	#Global.quota_reached.connect(show)
