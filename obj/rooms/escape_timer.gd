fd9d70d600708bfbbd6c0b7ac758822a68356fd1
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2025-07-03 02:44:20 +0300

    Fix hidden variables warning

--- obj/rooms/escape_timer.gd
+++ obj/rooms/escape_timer.gd
@@ -4,6 +4,6 @@ extends Timer
 var started = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if started:
 		label.text = str(snappedf(time_left, 0.01))
