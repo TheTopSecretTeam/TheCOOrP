e8545aa47640389ca33b84620033088feb05733d
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2025-07-06 22:39:36 +0300

    escape_menu fixes

--- UI/gafur_stuff/escape_menu.gd
+++ UI/gafur_stuff/escape_menu.gd
@@ -1,1 +1,10 @@
 extends Control
+
+# Inspired by this video https://youtu.be/JEQR4ALlwVU
+
+func _on_resume_pressed():
+	visible = false
+
+func _on_quit_pressed():
+	pass
+	#get_tree().quit() # TODO Gracefully quit the level
