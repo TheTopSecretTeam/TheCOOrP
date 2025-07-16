dc77ac3283d1314a5d10b29baf3d612fd3bd9f82
acecution <acecution7@gmail.com>
2025-07-16 19:54:06 +0300

    feat: baloon logic

--- obj/bars/baloon_bar.gd
+++ obj/bars/baloon_bar.gd
@@ -1,4 +1,24 @@
 extends AnomalyBar
+class_name BalloonBar
 
-func _on_bar_work_completed(pe_box: Variant) -> void:
-	work_completed.emit(pe_box)
+@onready var state_label = $StateLabel
+@onready var history_label = $HistoryLabel
+
+var interaction_history: Array[String] = []
+
+func _ready() -> void:
+	super._ready()
+	update_display()
+
+func work(action: AnomalyAction):
+	interaction_history.append(action.action_name + " (" + Time.get_time_string_from_system() + ")")
+	if interaction_history.size() > 5:  # Keep only last 5 interactions
+		interaction_history.pop_front()
+	update_display()
+	super.work(action)
+
+func update_display():
+	if chamber and chamber.anomaly and chamber.anomaly.has_method("get_state"):
+		var state = chamber.anomaly.get_state()
+		state_label.text = "State: " + ("RED" if state == 1 else "Normal")
+	history_label.text = "History:\n" + "\n".join(interaction_history)
