7cb20675314de568d8957ba789b6eb4dda62bb9c
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2025-07-17 20:42:30 +0300

    Main Menu refactoring
    It was overcrowded with unusable buttons

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -7,46 +7,17 @@ func _ready() -> void:
 		for btn in web_disable_list:
 			btn.disabled = true
 
-func _on_Main_NewGame_pressed():
-	pass
-	#_mok_jmp_to_map()
-	#$Main.hide()
-	#$NewGameMenu.show()
-
-func _on_Main_Load_pressed():
-	_mok_jmp_to_map()
-	#$Main.hide()
-	#$LoadMenu.show()
-
-func _on_Main_Settings_pressed():
-	print("Settings are not implemented")
-
-func _on_Main_Join_pressed():
-	get_tree().change_scene_to_file("res://scenes/map.tscn")
-
-func _on_Main_Exit_pressed():
-	$ExitDialog.popup_centered()
-
-func _on_Main_Exit_confirmed():
-	get_tree().quit()
-
-func _on_Load_NewWorld_pressed():
-	print("New World is not implemented")
-
-func _on_Load_Back_pressed():
-	$LoadMenu.hide()
-	$Main.show()
-
-func _on_NG_Back_pressed():
-	$NewGameMenu.hide()
-	$Main.show()
-
-func _on_NG_Start_pressed():
-	pass
-
-func _mok_jmp_to_map():
+func _on_Multiplayer_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/multiplayer.tscn")
 
+func _on_Singleplayer_pressed():
+	get_tree().change_scene_to_file("res://scenes/map.tscn")
 
-func _on_connect_button_pressed() -> void:
-	_mok_jmp_to_map()
+func _on_Settings_pressed():
+	print("Settings are not implemented")
+
+func _on_Exit_pressed():
+	$ExitDialog.popup_centered()
+
+func _on_Exit_confirmed():
+	get_tree().quit()
