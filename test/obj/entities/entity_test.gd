d8fe3d0023057b0007f619f8cdd7fe389be1fd5c
KarFayInn <fayzakhanovk@gmail.com>
2025-07-19 18:20:26 +0300

    Unittest refactored

--- test/obj/entities/entity_test.gd
+++ test/obj/entities/entity_test.gd
@@ -11,28 +11,30 @@ func test_combat() -> void:
 	var runner := scene_runner(scene)
 	var chamber = runner.invoke("find_child", "chamber")
 	chamber.agent_selected("Pong")
-	await await_millis(1000)
+	await await_millis(3000)
 	scene.queue_free()
 
 func test_death() -> void:
 	var scene = load("res://scenes/map.tscn").instantiate()
 	var runner := scene_runner(scene)
 	var chamber = runner.invoke("find_child", "chamber")
-	#var pong = runner.invoke("find_child", "corridor2")\
-					#.find_child("room_path").find_child("Agent2")
-	chamber.action(chamber.actions[2])
+	var work_manager = runner.invoke("find_child", "WorkSelectManager")
+	chamber.show_work()
+	work_manager.action(chamber.actions[2])
 	chamber.agent_selected("Pong")
-	await await_millis(1000)
+	await await_millis(3000)
 	scene.queue_free()
 	
 func test_kill() -> void:
 	var scene = load("res://scenes/map.tscn").instantiate()
 	var runner := scene_runner(scene)
 	var chamber = runner.invoke("find_child", "chamber")
+	var work_manager = runner.invoke("find_child", "WorkSelectManager")
 	var pong = runner.invoke("find_child", "corridor2")\
 					.find_child("room_path").find_child("Agent2")
 	pong.entity_resource.current_weapon.base_damage = 200
-	chamber.action(chamber.actions[2])
+	chamber.show_work()
+	work_manager.action(chamber.actions[2])
 	chamber.agent_selected("Pong")
-	await await_millis(1000)
+	await await_millis(3000)
 	scene.queue_free()
