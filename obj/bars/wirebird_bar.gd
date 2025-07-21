292134aeefed415466c285f3fc548aada77b6bc7
corndev <corndev@mail.ru>
2025-07-21 22:26:47 +0300

    wirebird escape sequence

--- obj/bars/wirebird_bar.gd
+++ obj/bars/wirebird_bar.gd
@@ -3,5 +3,5 @@ extends AnomalyBar
 func _on_bar_work_completed(pe_box: Variant) -> void:
 	work_completed.emit(pe_box)
 	#print(anomaly_action.action_name)
-	#if anomaly_action.action_name == "Freeze":
-		#chamber.escape()
+	if anomaly_action.action_name == "Repression":
+		chamber.escape()
